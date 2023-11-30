1d4fb3b52fc960fd61c322ca005f04965160cf5a
Steven Lawrence <54939714+SL-itw@users.noreply.github.com>
2023-11-30 15:00:03 -0500

    Added way to included multiple affiliations

--- getfunctions.R
+++ getfunctions.R
@@ -1,9 +1,28 @@
 
-get_pubmed<- function(name, hiredate){
+
+
+########################################
+
+# Gets publications
+get_pubmed<- function(name,
+                      hiredate,
+                         affiliation1 = NA,
+                        affiliation2 = NA,
+                       affiliation3 = NA
+                      ){
 
   # Formats author name
   author_name = name
-  query <- paste0(author_name, "[Author]")
+  if(is.na(affiliation1) & is.na(affiliation2) & is.na(affiliation3)){
+    query <- paste0(author_name, "[Author]")
+  }else if(is.na(affiliation2) & is.na(affiliation3)){
+    query <- paste0(author_name, "[Author]", " AND ", affiliation1,"[Affiliation]")
+  }else if(is.na(affiliation3)){
+    query <- paste0(author_name, "[Author]", " AND ","(", affiliation1,"[Affiliation]"," OR ", affiliation2,"[Affiliation]",")")
+  }else{
+    query <- paste0(author_name, "[Author]", " AND ","(", affiliation1,"[Affiliation]"," OR ", affiliation2,"[Affiliation]"," OR ", affiliation3,"[Affiliation]",")")
+  }
+
 
   # queries author article ID's by name
   ids <- entrez_search("pubmed", query, retmax = 10000)$ids
@@ -28,3 +47,8 @@ get_pubmed<- function(name, hiredate){
     rename("titles" = "text") %>%
     select(titles, date)
   }
+
+
+#################################
+
+
